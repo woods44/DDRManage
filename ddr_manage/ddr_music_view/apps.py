24b6d72b23fb35a1b1329ebad39c3e1eaf163d2e
woods <hutakikanata805782@gmail.com>
2019-01-08 23:39:07 +0900

    ファイルをすべて追加 index.htmlのタイトルをDDRMusicViewへ変更

---
+++ ddr_manage/ddr_music_view/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DdrMusicViewConfig(AppConfig):
+    name = 'ddr_music_view'
---
+++ ddr_manage/ddr_music_view/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'ddr/index.html')
